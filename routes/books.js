cb96b18b5d001e13d85e8a1046e0421c7bf9a814
Mario Ibarra <mario2014foto@gmail.com>
2020-04-01 16:31:26 +0200

    Books initial set Up

---
+++ routes/books.js
@@ -0,0 +1,43 @@
+
+const express = require('express')
+const router = express.Router()
+const Book = require('../models/book')
+const Author = require('../models/author')
+
+// All Books Route
+router.get('/', async (req, res) => {
+  res.send('All Books')
+})
+
+// New Book Route
+router.get('/new', async (req, res) => {
+  try {
+    const authors = await Author.find({})
+    const book = new Book()
+    res.render('books/new', {
+      authors: authors,
+      book: book
+    })
+  } catch (error) {
+    res. redirect('/books')
+  }
+})
+
+// Create Book Route
+router.post('/', async (req, res) => {
+  res.send('Create Book')
+})
+
+module.exports = router
+
+
+
+
+
+
+
+
+
+
+
+
