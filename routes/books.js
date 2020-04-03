89121b91c2c1d6cacc683ff5dbb0eaedc6e4ee20
Mario Ibarra <mario2014foto@gmail.com>
2020-04-03 21:00:36 +0200

    Finish books routes

--- routes/books.js
+++ routes/books.js
@@ -45,12 +45,83 @@ router.post('/', async (req, res) => {
 
   try {
     const newBook = await book.save()
-    // res.redirect(`books/${newBook.id}`)
-    res.redirect(`books`)
+    res.redirect(`books/${newBook.id}`)
+    
   } catch {
     renderNewPage(res, book, true)
   }
 })
+
+// Show Book Route
+router.get('/:id', async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id)
+                            .populate('author')
+                            .exec()
+    res.render('books/show', { book: book })
+  } catch  {
+    res.redirect('/')
+  }
+})
+
+// Edit Book Route
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id)
+   
+    renderEditPage(res, book)
+
+  } catch (error) {
+    res.redirect('/')
+  }
+})
+
+// Update Book Route
+router.put('/:id', async (req, res) => {
+  let book
+  try {
+    book = await Book.findById(req.params.id)
+    book.title = req.body.title,
+    book.author = req.body.author,
+    book.publishDate = new Date(req.body.publishDate),
+    book.pageCount = req.body.pageCount,
+    book.description = req.body.description
+    
+    if (req.body.cover != null && req.body.cover !== '') {
+      saveCover(book, req.body.cover)
+    }
+    await book.save()
+    res.redirect(`/books/${book.id}`)
+    
+  } catch {
+    if (book != null ) {
+      renderEditPage(res, book, true)
+    } else {
+      redirect('/')
+    }
+  }
+})
+
+// Route to delete book
+router.delete('/:id', async (req, res) => {
+  let book
+  try {
+    book = await Book.findById(req.params.id)
+    await book.remove()
+    res.redirect('/books')
+  } catch  {
+    if (book != null) {
+      res.render('books/show', {
+        book: book,
+        errorMessage: 'Could not remove book'
+      })
+    } else {
+      res.redirect('/')
+    }
+  }
+})
+
+//=== Functions =====
 
 async function renderNewPage(res, book, hasError = false) {
   renderFormPage(res, book, 'new', hasError)
@@ -88,5 +159,7 @@ function saveCover(book, coverEncoded) {
     book.coverImageType = cover.type
   }
 }
+
+
 
 module.exports = router
