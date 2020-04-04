d48fddd6b6e119ac5175bf6d09df4915c6213e28
Mario Ibarra <mario2014foto@gmail.com>
2020-04-04 20:31:47 +0200

    Books styles

--- routes/books.js
+++ routes/books.js
@@ -46,7 +46,6 @@ router.post('/', async (req, res) => {
   try {
     const newBook = await book.save()
     res.redirect(`books/${newBook.id}`)
-    
   } catch {
     renderNewPage(res, book, true)
   }
@@ -56,10 +55,10 @@ router.post('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const book = await Book.findById(req.params.id)
-                            .populate('author')
-                            .exec()
+                           .populate('author')
+                           .exec()
     res.render('books/show', { book: book })
-  } catch  {
+  } catch {
     res.redirect('/')
   }
 })
@@ -68,10 +67,8 @@ router.get('/:id', async (req, res) => {
 router.get('/:id/edit', async (req, res) => {
   try {
     const book = await Book.findById(req.params.id)
-   
     renderEditPage(res, book)
-
-  } catch (error) {
+  } catch {
     res.redirect('/')
   }
 })
@@ -79,22 +76,21 @@ router.get('/:id/edit', async (req, res) => {
 // Update Book Route
 router.put('/:id', async (req, res) => {
   let book
+
   try {
     book = await Book.findById(req.params.id)
-    book.title = req.body.title,
-    book.author = req.body.author,
-    book.publishDate = new Date(req.body.publishDate),
-    book.pageCount = req.body.pageCount,
+    book.title = req.body.title
+    book.author = req.body.author
+    book.publishDate = new Date(req.body.publishDate)
+    book.pageCount = req.body.pageCount
     book.description = req.body.description
-    
     if (req.body.cover != null && req.body.cover !== '') {
       saveCover(book, req.body.cover)
     }
     await book.save()
     res.redirect(`/books/${book.id}`)
-    
   } catch {
-    if (book != null ) {
+    if (book != null) {
       renderEditPage(res, book, true)
     } else {
       redirect('/')
@@ -102,14 +98,14 @@ router.put('/:id', async (req, res) => {
   }
 })
 
-// Route to delete book
+// Delete Book Page
 router.delete('/:id', async (req, res) => {
   let book
   try {
     book = await Book.findById(req.params.id)
     await book.remove()
     res.redirect('/books')
-  } catch  {
+  } catch {
     if (book != null) {
       res.render('books/show', {
         book: book,
@@ -120,8 +116,6 @@ router.delete('/:id', async (req, res) => {
     }
   }
 })
-
-//=== Functions =====
 
 async function renderNewPage(res, book, hasError = false) {
   renderFormPage(res, book, 'new', hasError)
@@ -159,7 +153,5 @@ function saveCover(book, coverEncoded) {
     book.coverImageType = cover.type
   }
 }
-
-
 
 module.exports = router
